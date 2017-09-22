4a0e5c8490c99abc7f32a2006fb48ea3d247bb54
Abu Sufian <sufian.milon@gmail.com>
2017-09-22 18:08:38 +0600

    finishing up

--- src/com/sufnom/Main.java
+++ src/com/sufnom/Main.java
@@ -1,9 +1,67 @@
 package com.sufnom;
 
+import com.sufnom.stack.StackRequest;
+import com.sufnom.sys.Config;
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.URLDecoder;
+import java.util.Base64;
+
+@SuppressWarnings("WeakerAccess")
 public class Main {
     public static final boolean debug = true;
+    public static final int PORT =
+            Integer.parseInt(Config.getSession().getValue(Config.KEY_STACK_PORT));
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)  {
+        try {
+            System.out.println("Stack Path : " + Config.getSession().getValue(Config.KEY_STACK_PATH));
+            onStart();
+        }
+        catch (Exception e){e.printStackTrace();}
+    }
+
+    @SuppressWarnings({"unused", "WeakerAccess"})
+    static void onStart() throws Exception{
+        HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
+        HttpContext context = server.createContext("/", new DefaultHandler());
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Listening to " + PORT);
+    }
+
+    static class DefaultHandler implements HttpHandler {
+        public void handle(HttpExchange t){
+            try {
+                String encoded_request = t.getRequestURI().getPath();
+                byte[] headerBytes = Base64.getDecoder().decode(
+                        URLDecoder.decode(encoded_request,"utf-8"));
+                StackRequest request = new StackRequest(headerBytes);
+                request.processRequest();
+                sendResponse(t, 200, request.getResponse());
+            }
+            catch (Exception e){
+                e.printStackTrace();
+                sendResponse(t, 200, "error".getBytes());
+            }
+        }
+
+        @SuppressWarnings("SameParameterValue")
+        private void sendResponse(HttpExchange t, int status, byte[] rawResponse){
+            try {
+                if (debug) System.out.println(status + " : length=" + rawResponse.length);
+                t.getResponseHeaders().set("Content-Type","application/octet-stream");
+                t.sendResponseHeaders(status, rawResponse.length);
+                OutputStream os = t.getResponseBody();
+                os.write(rawResponse);
+                os.close();
+            }
+            catch (Exception e){e.printStackTrace();}
+        }
     }
 }
--- src/com/sufnom/stack/StackTerminal.java
+++ src/com/sufnom/stack/StackTerminal.java
@@ -1,10 +1,103 @@
 package com.sufnom.stack;
 
+import com.sufnom.stack.def.DynamicStackInterface;
+import com.sufnom.stack.def.FixedStackInterface;
+
+import java.nio.ByteBuffer;
 import java.util.HashMap;
 
 public class StackTerminal {
     private static final StackTerminal session = new StackTerminal();
     public static StackTerminal getSession() { return session; }
 
-    private HashMap<String, Object> connectedStacks;
+    public static final String COMMAND_INSERT = "insert";
+    public static final String COMMAND_GET = "get";
+    public static final String COMMAND_UPDATE = "update";
+
+    public static final String TYPE_FIXED_STACK = "fsi";
+    public static final String TYPE_EXTENDED_STACK = "esi";
+    public static final String TYPE_DYNAMIC_STACK = "dsi";
+
+    private HashMap<String, Object> connectedStacks = new HashMap<>();
+
+    public byte[] processCommand(String command, String target, String type, byte[] dataBytes){
+        Object stack = connectedStacks.get(target);
+        if (stack == null){
+            stack = connectToStack(target, type);
+            if (stack == null) return "error".getBytes();
+            connectedStacks.put(target, stack);
+        }
+        try {
+            switch (type){
+                case TYPE_FIXED_STACK:
+                    return processFixedStack((FixedStackInterface)stack,
+                            command, dataBytes);
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return "error".getBytes();
+        }
+        return new byte[0];
+    }
+
+    private byte[] processFixedStack(FixedStackInterface stack,
+                         String command, byte[] dataBytes) throws Exception{
+        switch (command){
+            case COMMAND_INSERT:
+                return convertToBytes(stack.insert(dataBytes));
+            case COMMAND_UPDATE:
+                byte[] data = new byte[4096];
+                System.arraycopy(dataBytes,8,data,0, data.length);
+                stack.updateBlock(getBlockId(dataBytes), data);
+                return "ok".getBytes();
+            case COMMAND_GET:
+                return stack.readBlock(getBlockId(dataBytes));
+        }
+        throw new Exception("Command Not Matched");
+    }
+
+    private byte[] convertToBytes(long blockId){
+        ByteBuffer buffer = ByteBuffer.allocate(8);
+        buffer.putLong(0, blockId);
+        byte[] result = buffer.array();
+        buffer.clear();
+        return result;
+    }
+
+    private long getBlockId(byte[] dataBlock){
+        ByteBuffer buffer = ByteBuffer.allocate(8);
+        byte[] blockHead = new byte[8];
+        System.arraycopy(dataBlock,0,blockHead,0,8);
+        buffer.put(blockHead);
+        long blockId = buffer.getLong(0);
+        buffer.clear();
+        return blockId;
+    }
+
+    private byte[] processExtendedStack(FixedStackInterface stack,
+                        String command, byte[] dataBytes) throws Exception{
+        return null;
+    }
+
+    private byte[] processDynamicStack(DynamicStackInterface stack,
+                       String command, byte[] dataBytes) throws Exception{
+        switch (command){
+            case COMMAND_INSERT:
+                return convertToBytes(stack.insert(dataBytes));
+            case COMMAND_GET:
+                return stack.readBlock(getBlockId(dataBytes));
+        }
+        throw new Exception("Command Not Matched");
+    }
+
+    private Object connectToStack(String target, String type){
+        switch (type){
+            case TYPE_FIXED_STACK: return new FixedStackInterface(target);
+            case TYPE_DYNAMIC_STACK: return new DynamicStackInterface(target);
+            //TODO need to add extended stacks
+            case TYPE_EXTENDED_STACK: return null;
+        }
+        return null;
+    }
 }
